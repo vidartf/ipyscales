233e3be677997b3cb84184564ddba8dc2f9faf04
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-09-17 19:07:38 +0200

    Small refactoring

--- packages/jupyter-scales/src/scale.ts
+++ packages/jupyter-scales/src/scale.ts
@@ -16,7 +16,7 @@ import {
 } from 'jupyter-dataserializers';
 
 import {
-  JUPYTER_EXTENSION_VERSION
+  JUPYTER_EXTENSION_VERSION, MODULE_NAME
 } from './version';
 
 import ndarray = require('ndarray');
@@ -201,7 +201,7 @@ abstract class ScaleModel extends WidgetModel {
   static serializers: ISerializerMap = WidgetModel.serializers;
 
   static model_name: string;    // Base model should not be instantiated directly
-  static model_module = 'jupyter-threeplot';
+  static model_module = MODULE_NAME;
   static model_module_version = JUPYTER_EXTENSION_VERSION;
   static view_name = null;
   static view_module = null;
--- packages/jupyter-scales/src/version.ts
+++ packages/jupyter-scales/src/version.ts
@@ -9,5 +9,6 @@
  * Update this value when attributes are added/removed from
  * your models, or serialized format changes.
  */
-export
-const JUPYTER_EXTENSION_VERSION = '1.0.0';
+export const JUPYTER_EXTENSION_VERSION = '1.0.0';
+
+export const MODULE_NAME = 'jupyter-scales';
