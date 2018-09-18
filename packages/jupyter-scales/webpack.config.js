57782238a3e2d333f671e0647007ada6759004ad
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-09-18 11:36:42 +0200

    Finish color bar transition, and other cleanup

--- packages/jupyter-scales/webpack.config.js
+++ packages/jupyter-scales/webpack.config.js
@@ -1,23 +1,62 @@
-var rules = [
+
+const path = require('path');
+const version = require('./package.json').version;
+
+const rules = [
   { test: /\.ts$/, loader: 'ts-loader' },
-  { test: /\.json$/, loader: 'json-loader' },
   { test: /\.js$/, loader: "source-map-loader" },
 ];
 
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'index.js',
-    path: __dirname + '/../../ipyscales/nbextension/static',
-    libraryTarget: 'amd'
+const externals = ['@jupyter-widgets/base'];
+
+module.exports = [
+  {
+    entry: './src/index.ts',
+    output: {
+      filename: 'index.js',
+      path: __dirname + '/../../ipyscales/nbextension/static',
+      libraryTarget: 'amd'
+    },
+    module: {
+      rules: rules
+    },
+    devtool: 'source-map',
+    externals,
+    resolve: {
+      // Add '.ts' as resolvable a extension.
+      extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+    }
   },
-  module: {
-    rules: rules
-  },
-  devtool: 'source-map',
-  externals: ['@jupyter-widgets/base'],
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+  {// Embeddable jupyter-scales bundle
+    //
+    // This bundle is generally almost identical to the notebook bundle
+    // containing the custom widget views and models.
+    //
+    // The only difference is in the configuration of the webpack public path
+    // for the static assets.
+    //
+    // It will be automatically distributed by unpkg to work with the static
+    // widget embedder.
+    //
+    // The target bundle is always `dist/index.js`, which is the path required
+    // by the custom widget embedder.
+    //
+    entry: './src/index.ts',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'amd',
+        library: "jupyter-scales",
+        publicPath: 'https://unpkg.com/jupyter-scales@' + version + '/dist/'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: rules
+    },
+    externals,
+    resolve: {
+      // Add '.ts' as resolvable extensions.
+      extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+    },
   }
-};
+];
